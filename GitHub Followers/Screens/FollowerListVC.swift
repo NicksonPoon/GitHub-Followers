a51ab32c5541fec07789dcaa540e48fcf2ed5cbc
nicksonpoon <thenicksonpoon@gmail.com>
2020-05-18 15:16:10 -0400

    Addd UICollectionView flow layout

--- GitHub Followers/Screens/FollowerListVC.swift
+++ GitHub Followers/Screens/FollowerListVC.swift
@@ -32,20 +32,32 @@ class FollowerListVC: UIViewController {
     }
     
     func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnflowLayout())
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
 
-    func getFollowers() {
+    func createThreeColumnflowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let avaliableWidth = width - (padding * 2) - (minimumItemSpacing * 2 ) //  * 2 because of Padding on left and right of screen
+        let itemWidth = avaliableWidth / 3
         
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+    func getFollowers()  {
         NetworkManager.shared.getFollowers(for: username, page: 1) { result in
             
             switch result {
             case .success(let followers):
                 print(followers)
-
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad stuff Happened", message: error.rawValue , buttonTitle: "Ok")
             }
