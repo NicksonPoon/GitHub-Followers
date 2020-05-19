18c7baeebc6e52c8d2bf0a695a875bb531d3bdaa
nicksonpoon <thenicksonpoon@gmail.com>
2020-05-19 18:55:49 -0400

    Project Organization

---
+++ GitHub Followers/Custom Views/Views/GFEmptyStateView.swift
@@ -0,0 +1,13 @@
+//
+//  GFEmptyStateView.swift
+//  GitHub Followers
+//
+//  Created by Nickson Poon on 5/19/20.
+//  Copyright © 2020 Nickson Poon. All rights reserved.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+    
+}
