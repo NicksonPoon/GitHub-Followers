6a9f818acd40933b1723874b1d5699cc1dc1179a
nicksonpoon <thenicksonpoon@gmail.com>
2020-05-19 20:02:28 -0400

    Add empty state view

--- GitHub Followers/Extensions/UIViewController+Ext.swift
+++ GitHub Followers/Extensions/UIViewController+Ext.swift
@@ -51,4 +51,10 @@ extension UIViewController {
 
     }
     
+    func showEmptyStateView(with message: String, in view: UIView){
+        let emptyStateView = GFEmptyStateView(message: message)
+        emptyStateView.frame = view.bounds
+        view.addSubview(emptyStateView)
+    }
+    
 }
--- GitHub Followers/Screens/FollowerListVC.swift
+++ GitHub Followers/Screens/FollowerListVC.swift
@@ -65,6 +65,15 @@ class FollowerListVC: UIViewController {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user doesn't have any followers 😢."
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                        return
+                    }
+                }
+                
                 self.updatedata()
                 
             case .failure(let error):
